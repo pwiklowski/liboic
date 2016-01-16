71c3c8d2923cd04e7fc40810914191dcffa5bdc0
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-01-16 19:16:00 +0100

    Add cbor library

---
+++ cbor.h
@@ -0,0 +1,269 @@
+#ifndef CBOR_H
+#define CBOR_H
+
+#include <string>
+#include <vector>
+#include "map"
+
+using namespace std;
+
+enum CborType_t {
+    CBOR_TYPE_UNSIGNED,
+    CBOR_TYPE_NEGATIVE,
+    CBOR_TYPE_BINARY,
+    CBOR_TYPE_STRING,
+    CBOR_TYPE_ARRAY,
+    CBOR_TYPE_MAP,
+    CBOR_TYPE_TAGGED,
+    CBOR_TYPE_SIMPLE,
+    CBOR_TYPE_FLOAT
+};
+
+
+class cbor{
+public:
+
+    static cbor* parse(vector<uint8_t>* data, uint16_t* p = 0){
+        uint16_t pointer;
+        if (p == 0)
+            pointer = 0;
+        else
+            pointer = *p;
+
+        uint8_t type = data->at(pointer++);
+        CborType_t majorType = (CborType_t)(type >> 5);
+        uint8_t minorType = (unsigned char) (type & 31);
+
+        uint32_t value =0;
+        if(minorType < 24) {
+            value = minorType;
+        } else if(minorType == 24) { // 1 byte
+            value = data->at(pointer++);
+        } else if(minorType == 25) { // 2 byte
+            value = ((unsigned short) data->at(pointer) << 8) | ((unsigned short) data->at(pointer+ 1));
+            pointer += 2;
+        } else if(minorType == 26) { // 4 byte
+            value = ((unsigned int) data->at(pointer) << 24) |
+                  ((unsigned int) data->at(pointer+ 1) << 16) |
+                  ((unsigned int) data->at(pointer+ 2) << 8 ) |
+                  ((unsigned int) data->at(pointer+ 3));
+            pointer +=4;
+        } else {
+
+        }
+
+        cbor* aa = new cbor(majorType);
+
+        if (majorType == CBOR_TYPE_ARRAY){
+            for (uint16_t i=0; i<value; i++)
+                aa->append(cbor::parse(data, &pointer));
+        }else if (majorType == CBOR_TYPE_STRING){
+            for (uint16_t i=0; i<value; i++)
+                aa->data()->push_back(data->at(pointer++));
+        }else if (majorType == CBOR_TYPE_MAP){
+            for (uint16_t i=0; i<value; i++){
+                cbor* key = cbor::parse(data, &pointer);
+                cbor* value = cbor::parse(data, &pointer);
+                aa->append(key, value);
+            }
+        }
+
+        if (p!=0)
+            *p = pointer;
+
+
+        return aa;
+    }
+    ~cbor(){
+        m_data.clear();
+
+        for(uint16_t i=0;i<m_array.size();i++){
+            delete m_array.at(i);
+        }
+        for(auto key = m_map.begin(); key!=m_map.end(); key++){
+            pair<cbor*, cbor*> p = *key;
+
+            delete p.first;
+            delete p.second;
+        }
+
+    }
+
+    cbor(string str) {
+        m_type = CBOR_TYPE_STRING;
+        for(uint16_t i=0; i<str.size();i++)
+            m_data.push_back(str.at(i));
+    }
+
+    cbor(long long val) {
+        if (val < 0)
+            m_type = CBOR_TYPE_NEGATIVE;
+        else
+            m_type = CBOR_TYPE_UNSIGNED;
+
+        //, val);
+
+
+        //-(val+1));
+
+    }
+
+    cbor(vector<cbor*> array) {
+        m_type = CBOR_TYPE_ARRAY;
+        m_array = array;
+    }
+
+    static cbor* array(){
+        return new cbor(CBOR_TYPE_ARRAY);
+    }
+
+    static cbor* map(){
+        return new cbor(CBOR_TYPE_MAP);
+    }
+
+    cbor(CborType_t type){
+        m_type = type;
+    }
+
+    CborType_t getType(){
+        return m_type;
+    }
+
+    vector< pair<cbor*, cbor*> >* toMap() {
+        if (m_type == CBOR_TYPE_MAP)
+            return &m_map;
+        else
+            return 0;
+    }
+
+
+   cbor* getMapValue(string key) {
+       if (m_type != CBOR_TYPE_MAP)
+           return 0;
+
+       for(auto item = m_map.begin();item !=m_map.end(); item++){
+           pair<cbor*, cbor*> p = *item ;
+
+           if (p.first->compare(key)){
+                return p.second;
+           }
+       }
+       return 0;
+
+    }
+
+   //workd only for strings
+    bool compare(string str){
+        if (str.size() != m_data.size())
+            return false;
+
+        for (uint16_t i=0; i<str.size(); i++){
+            if (str[i] != m_data[i])
+                return false;
+        }
+        return true;
+    }
+
+    void append(cbor* key, cbor* value){
+        m_map.push_back(make_pair(key, value));
+    }
+    void append(cbor* value){
+        m_array.push_back(value);
+    }
+
+    vector<cbor*>* toArray() {
+        if (m_type == CBOR_TYPE_ARRAY)
+            return &m_array;
+        else
+            return 0;
+    }
+
+    string toString(){
+        string out;
+        for(uint16_t i=0; i<m_data.size();i++)
+            out += m_data.at(i);
+        return out;
+    }
+
+
+    vector<uint8_t>* data(){
+        return &m_data;
+    }
+
+
+    void dump(vector<uint8_t>* data){
+       uint8_t majorType = m_type << 5;
+
+       unsigned long long value = 0;
+       if (m_type == CBOR_TYPE_ARRAY){
+           value = m_array.size();
+       }else if (m_type == CBOR_TYPE_MAP){
+           value = m_map.size();
+       }else if (m_type == CBOR_TYPE_STRING){
+           value = m_data.size();
+       }
+
+       if(value < 24ULL) {
+           data->push_back(majorType | value);
+       } else if(value < 256ULL) {
+           data->push_back(majorType | 24);
+           data->push_back(value);
+       } else if(value < 65536ULL) {
+           data->push_back(majorType | 25);
+           data->push_back(value >> 8);
+       } else if(value < 4294967296ULL) {
+           data->push_back(majorType | 26);
+           data->push_back(value >> 24);
+           data->push_back(value >> 16);
+           data->push_back(value >> 8);
+           data->push_back(value);
+       } else {
+           data->push_back(majorType | 27);
+           data->push_back(value >> 56);
+           data->push_back(value >> 48);
+           data->push_back(value >> 40);
+           data->push_back(value >> 32);
+           data->push_back(value >> 24);
+           data->push_back(value >> 16);
+           data->push_back(value >> 8);
+           data->push_back(value);
+       }
+
+       if (m_type == CBOR_TYPE_ARRAY){
+           for(uint16_t i=0;i<m_array.size();i++){
+               m_array.at(i)->dump(data);
+           }
+       } else if (m_type == CBOR_TYPE_MAP){
+           for(auto key = m_map.begin(); key!=m_map.end(); key++){
+               pair<cbor*, cbor*> p = *key;
+
+               p.first->dump(data);
+               p.second->dump(data);
+           }
+       } else if (m_type == CBOR_TYPE_STRING){
+            for(uint16_t i=0; i<m_data.size();i++)
+                data->push_back(m_data.at(i));
+
+       }
+
+
+
+    }
+
+    bool operator <(const cbor& rhs) const
+    {
+        return true;
+    }
+
+private:
+    CborType_t m_type;
+
+    vector<uint8_t> m_data;
+
+
+    vector<cbor*> m_array;
+    vector< pair<cbor*, cbor*> > m_map;
+};
+
+
+#endif // cbor_H
