a237ce9cea25c4369874334353392970d3b2595d
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-01-24 09:44:23 +0100

    Add ifdefs for support ipv6 and ipv4

--- OICServer.cpp
+++ OICServer.cpp
@@ -122,8 +122,10 @@ void OICServer::start(string ip, string interface){
     }
 
     pthread_create(&m_thread, NULL, &OICServer::run, this);
-    pthread_create(&m_discoveryThread, NULL, &OICServer::runDiscovery, this);
 
+#ifdef IPV6
+    pthread_create(&m_discoveryThread, NULL, &OICServer::runDiscovery, this);
+#endif
 }
 
 void OICServer::stop(){
@@ -134,20 +136,41 @@ void* OICServer::run(void* param){
     OICServer* oic_server = (OICServer*) param;
     COAPServer* coap_server = oic_server->getCoapServer();
 
-    int fd = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
-    oic_server->setSocketFd(fd);
 
-    struct sockaddr_in6 serv, client;
     const int on = 1;
+#ifdef IPV6
+    int fd = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    struct sockaddr_in6 serv, client;
     serv.sin6_family = AF_INET6;
     serv.sin6_port = htons(5683);
     serv.sin6_scope_id = if_nametoindex(coap_server->getInterface().c_str());
-
 
     if (inet_pton(AF_INET6, coap_server->getIp().c_str(), &(serv.sin6_addr)) < 0){
         log("Unable to bind");
         return 0;
     }
+#endif
+
+
+#ifdef IPV4
+    int fd = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
+    struct sockaddr_in serv,client;
+    struct ip_mreq mreq;
+
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(5683);
+    serv.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    mreq.imr_multiaddr.s_addr=inet_addr("224.0.1.187");
+    mreq.imr_interface.s_addr=htonl(INADDR_ANY);
+    if (setsockopt(fd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq)) < 0) {
+        return 0;
+    }
+#endif
+
+    oic_server->setSocketFd(fd);
+
     uint8_t buffer[1024];
     socklen_t l = sizeof(client);
     if(setsockopt(oic_server->getSocketFd(), SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
@@ -172,7 +195,7 @@ void* OICServer::run(void* param){
     }
 }
 
-
+#ifdef IPV6
 void* OICServer::runDiscovery(void* param){
     OICServer* oic_server = (OICServer*) param;
     COAPServer* coap_server = oic_server->getCoapServer();
@@ -223,9 +246,11 @@ void* OICServer::runDiscovery(void* param){
         coap_server->handleMessage(p);
     }
 }
+#endif
 
+
+#ifdef IPV6
 string OICServer::convertAddress(sockaddr_in6 addr){
-
     string port = to_string(htons(addr.sin6_port));
     char a[30];
 
@@ -239,7 +264,25 @@ string OICServer::convertAddress(sockaddr_in6 addr){
     address += port;
     return address;
 }
+#endif
 
+
+#ifdef IPV4
+string OICServer::convertAddress(sockaddr_in addr){
+    string port = to_string(htons(addr.sin_port));
+    char a[30];
+
+    if (inet_ntop(AF_INET, (sockaddr_in*)&addr.sin_addr, a, 30) == 0){
+        log("Unable to bind");
+        return 0;
+    }
+
+    string address(a);
+    address += ' ';
+    address += port;
+    return address;
+}
+#endif
 
 OICResource* OICServer::getResource(string href){
     for(uint16_t i=0; i<m_resources.size();i++){
@@ -255,6 +298,8 @@ void OICServer::send(string destination, COAPPacket* packet, COAPResponseHandler
     string ip = destination.substr(0, pos);
     uint16_t port = atoi(destination.substr(pos).c_str());
 
+#ifdef IPV6
+
     struct sockaddr_in6 client;
     client.sin6_family = AF_INET6;
     client.sin6_port = htons(port);
@@ -263,10 +308,30 @@ void OICServer::send(string destination, COAPPacket* packet, COAPResponseHandler
     if (inet_pton(AF_INET6, ip.c_str(), &(client.sin6_addr)) < 0){
         log("err");
     }
+#endif
+
+#ifdef IPV4
+
+    struct sockaddr_in client;
+    client.sin_family = AF_INET;
+    client.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip.c_str(), &(client.sin_addr)) < 0){
+        log("err");
+    }
+#endif
+
 
     send(client, packet, func);
 }
+#ifdef IPV6
 void OICServer::send(sockaddr_in6 destination, COAPPacket* packet, COAPResponseHandler func){
+#endif
+
+#ifdef IPV4
+void OICServer::send(sockaddr_in destination, COAPPacket* packet, COAPResponseHandler func){
+#endif
+
     uint8_t buffer[1024];
     size_t response_len;
     socklen_t l = sizeof(destination);
--- OICServer.h
+++ OICServer.h
@@ -7,6 +7,11 @@
 #include "cbor.h"
 #include <netinet/in.h>
 
+
+#define IPV4
+
+
+
 typedef void(*OICServerSendData)(uint8_t*, uint16_t);
 
 class OICResource;
@@ -22,13 +27,27 @@ public:
     void addResource(OICResource* res) {res->setCoapServer(&coap_server); m_resources.push_back(res);}
 
     void send(string destination, COAPPacket* packet, COAPResponseHandler func);
-    void send(sockaddr_in6 destination, COAPPacket* packet, COAPResponseHandler func);
+#ifdef IPV6
+void send(sockaddr_in6 destination, COAPPacket* packet, COAPResponseHandler func);
+#endif
+
+#ifdef IPV4
+void send(sockaddr_in destination, COAPPacket* packet, COAPResponseHandler func);
+#endif
+
     COAPServer* getCoapServer() { return &coap_server; }
 
     int  getSocketFd() { return m_socketFd; }
     void setSocketFd(int socket) { m_socketFd = socket; }
 private:
+
+#ifdef IPV6
     string convertAddress(sockaddr_in6 addr);
+#endif
+
+#ifdef IPV4
+    string convertAddress(sockaddr_in addr);
+#endif
     static void* run(void*param);
     static void* runDiscovery(void*param);
     bool onRequest(COAPServer* server, COAPPacket* request, COAPPacket* response);
